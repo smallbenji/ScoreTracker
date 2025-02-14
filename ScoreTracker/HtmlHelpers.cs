491d63d03d2d12defc041e7994d3ec81a4dfcf57
smallbenji <benjaminkfalch@gmail.com>
2025-02-14 23:08:20 +0100

    Html Helpers

---
+++ ScoreTracker/HtmlHelpers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ScoreTracker
+{
+    public static class HtmlHelpers
+    {
+        public static IEnumerable<SelectListItem> GetSelections(this IHtmlHelper htmlHelper, string selectedViewData)
+        {
+            return htmlHelper.ViewData[selectedViewData] as IEnumerable<SelectListItem>;
+        }
+    }
+}
